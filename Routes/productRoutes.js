cf513f048d3fcba3546624212f733af29bbd537f
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-05-28 19:11:33 +0500

    Add images gallery route for uploading multiple images

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -2,8 +2,45 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../Controllers/productController");
 const { auth } = require("../Middleware/auth");
+const multer = require("multer");
 
-router.post("/add-product", auth, controller.addProduct);
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("Invalid file type");
+
+    if (isValid) {
+      uploadError = null;
+    }
+    cb(uploadError, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(" ").join("-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extension}`);
+  },
+});
+
+const uploadOptions = multer({ storage: storage });
+
+router.post(
+  "/add-product",
+  auth,
+  uploadOptions.single("image"),
+  controller.addProduct
+);
+router.put(
+  "/image-gallery/:id",
+  auth,
+  uploadOptions.array("images", 10),
+  controller.imageGallery
+);
 router.delete("/delete-product/:id", auth, controller.deleteProduct);
 router.put("/update-product/:id", auth, controller.updateProduct);
 router.get("/show-products", controller.showProducts);
