589412ee2049fe9ad479e3601d8a82753f4d1b6c
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-07-10 12:56:34 +0500

    Configure multer for uplaoding static images

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,11 +2,42 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../Controllers/userController");
 const { auth } = require("../Middleware/auth");
+const multer = require("multer");
 
-router.post("/signup", controller.signup);
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("Invalid file type");
+
+    if (isValid) {
+      uploadError = null;
+    }
+    cb(uploadError, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(" ").join("-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extension}`);
+  },
+});
+
+const uploadOptions = multer({ storage: storage });
+
+router.post("/signup", uploadOptions.single("pic"), controller.signup);
 router.post("/login", controller.login);
 router.get("/get-user", auth, controller.getUser);
-router.put("/update-user/:id", auth, controller.updateUser);
+router.put(
+  "/update-user/:id",
+  auth,
+  uploadOptions.single("pic"),
+  controller.updateUser
+);
 router.delete("/delete-account/:id", auth, controller.deleteAccount);
 
 module.exports = router;
